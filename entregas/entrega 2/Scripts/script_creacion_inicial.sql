8978ca423fefe80e422bb51d9116dd2a2ffed853
ManuRodri01 <129609340+ManuRodri01@users.noreply.github.com>
2026-05-23 10:52:41 -0300

    Creacion De tablas y Scripts
    Se crearon las tablas y las migraciones de: Agente, Cliente, Solicitud, Solicitud_X_Ciudad y Propuesta

--- entregas/entrega 2/Scripts/script_creacion_inicial.sql
+++ entregas/entrega 2/Scripts/script_creacion_inicial.sql
@@ -36,7 +36,37 @@ DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Excursiones;
 IF EXISTS (SELECT name FROM sys.procedures WHERE name = 'migrar_Agencias')
 DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Agencias;
 
+IF EXISTS (SELECT name FROM sys.procedures WHERE name = 'migrar_Agentes')
+DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Agentes;
+
+IF EXISTS (SELECT name FROM sys.procedures WHERE name = 'migrar_Clientes')
+DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Clientes;
+
+IF EXISTS (SELECT name FROM sys.procedures WHERE name = 'migrar_Solicitudes')
+DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Solicitudes;
+
+IF EXISTS (SELECT name FROM sys.procedures WHERE name = 'migrar_Solicitudes_X_Ciudad')
+DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Solicitudes_X_Ciudad;
+
+IF EXISTS (SELECT name FROM sys.procedures WHERE name = 'migrar_Propuestas')
+DROP PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Propuestas;
+
 --DROP Preventivo de Tablas OLTP------------------------------------------------------
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'Propuesta')
+DROP TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Propuesta;
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'Solicitud_X_Ciudad')
+DROP TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud_X_Ciudad;
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'Solicitud')
+DROP TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud;
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'Cliente')
+DROP TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Cliente;
+
+IF EXISTS (SELECT name FROM sys.tables WHERE name = 'Agente')
+DROP TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Agente;
 
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'Agencia')
 DROP TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Agencia;
@@ -161,6 +191,76 @@ CREATE TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Agencia (
 	Agencia_Mail NVARCHAR(max) not null,
 	Agencia_ID_Direccion BIGINT not null,
 	FOREIGN KEY (Agencia_ID_Direccion) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Direccion(Direccion_ID)
+);
+
+CREATE TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Agente (
+	Agente_ID BIGINT IDENTITY(1,1) PRIMARY KEY,
+	Agente_Legajo bigint not null,
+	Agente_Nombre NVARCHAR(510) not null,
+	Agente_Apellido NVARCHAR(510) not null,
+	Agente_DNI NVARCHAR(510) not null,
+	Agente_Fecha_Nac date not null,
+	Agente_Telefono NVARCHAR(510) not null,
+	Agente_Mail NVARCHAR(510) not null,
+	Agente_ID_Direccion BIGINT not null,
+	Agente_ID_Agencia BIGINT not null,
+	FOREIGN KEY (Agente_ID_Direccion) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Direccion(Direccion_ID),
+	FOREIGN KEY (Agente_ID_Agencia) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Agencia(Agencia_ID)
+);
+
+CREATE TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Cliente (
+	Cliente_ID BIGINT IDENTITY(1,1) PRIMARY KEY,
+	Cliente_Nombre NVARCHAR(510) not null,
+	Cliente_Apellido NVARCHAR(510) not null,
+	Cliente_DNI NVARCHAR(510) not null,
+	Cliente_Fecha_Nac date not null,
+	Cliente_Telefono NVARCHAR(510) not null,
+	Cliente_Mail NVARCHAR(510) not null,
+	Cliente_ID_Direccion BIGINT not null,
+	FOREIGN KEY (Cliente_ID_Direccion) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Direccion(Direccion_ID)
+);
+
+CREATE TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud (
+	Solicitud_ID BIGINT IDENTITY(1,1) PRIMARY KEY,
+	Solicitud_Nro_Solicitud bigint not null,
+	Solicitud_Fecha_Solicitud date not null,
+	Solicitud_Fecha_Inicio_Tentativa date not null,
+	Solicitud_Fecha_Fin_Tentativa date not null,
+	Solicitud_Cantidad_Pasajeros int not null,
+	Solicitud_Observaciones NVARCHAR(max) not null,
+	Solicitud_Presupuesto_Estimado decimal(18,2) not null,
+	Solicitud_ID_Cliente BIGINT not null,
+	Solicitud_ID_Agente BIGINT not null,
+	FOREIGN KEY (Solicitud_ID_Cliente) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Cliente(Cliente_ID),
+	FOREIGN KEY (Solicitud_ID_Agente) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Agente(Agente_ID)
+);
+
+CREATE TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud_X_Ciudad (
+	Solicitud_X_Ciudad_ID BIGINT IDENTITY(1,1) PRIMARY KEY,
+	Solicitud_X_Ciudad_Cantidad_Dias int not null,
+	Solicitud_X_Ciudad_Observaciones NVARCHAR(max) not null,
+	Solicitud_X_Ciudad_ID_Solicitud BIGINT not null,
+	Solicitud_X_Ciudad_ID_Ciudad BIGINT not null,
+	FOREIGN KEY (Solicitud_X_Ciudad_ID_Solicitud) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud(Solicitud_ID),
+	FOREIGN KEY (Solicitud_X_Ciudad_ID_Ciudad) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Ciudad(Ciudad_ID)
+);
+
+CREATE TABLE GANEN_LA_CUARTA_O_NO_VUELVAN.Propuesta (
+	Propuesta_ID BIGINT IDENTITY(1,1) PRIMARY KEY,
+	Propouesta_Nro_Propouesta bigint not null,
+	Propuesta_Fecha_Emision date not null,
+	Propuesta_Fecha_Vigencia_Hasta date not null,
+	Propuesta_Fecha_Desde date not null,
+	Propuesta_Fecha_Hasta date not null,
+	Propouesta_Subtotal decimal(18,2) not null,
+	Propuesta_Descuento decimal(18,2) not null,
+	Propuesta_Importe_Total decimal(18,2) not null,
+	Propuesta_ID_Solicitud BIGINT not null,
+	Propuesta_ID_Agente BIGINT not null,
+	Propuesta_ID_Estado BIGINT not null,
+	FOREIGN KEY (Propuesta_ID_Solicitud) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud(Solicitud_ID),
+	FOREIGN KEY (Propuesta_ID_Agente) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Agente(Agente_ID),
+	FOREIGN KEY (Propuesta_ID_Estado) REFERENCES GANEN_LA_CUARTA_O_NO_VUELVAN.Estado_Propuesta(Estado_Propuesta_ID)
 );
 
 GO
@@ -345,6 +445,173 @@ CREATE PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Agencias
 	END
 GO
 
+CREATE PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Agentes
+ AS
+	BEGIN
+		INSERT INTO GANEN_LA_CUARTA_O_NO_VUELVAN.Agente(Agente_Legajo, Agente_Nombre, Agente_Apellido, Agente_Mail, Agente_DNI, Agente_Fecha_Nac, Agente_Telefono, Agente_ID_Direccion, Agente_ID_Agencia)
+		SELECT DISTINCT t.Agente_Legajo, t.Agente_Nombre, t.Agente_Apellido, t.Agente_Mail, t.Agente_Dni, t.Agente_Fecha_Nac, t.Agente_Telefono, d.Direccion_ID, a.Agencia_ID
+		FROM gd_esquema.Maestra t
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Provincia p ON p.Provincia_Nombre = t.Agente_Provincia
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Localidad l on l.Localidad_ID_Provincia = p.Provincia_ID and l.Localidad_Nombre = t.Agente_Localidad
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Direccion d ON d.Direccion_ID_Localidad = l.Localidad_ID and d.Direccion = t.Agente_Direccion
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Agencia a 
+			on a.Agencia_Nro_Agencia = t.Agencia_Nro_Agencia
+			AND a.Agencia_Telefono = t.Agencia_Telefono
+			AND a.Agencia_Mail = t.Agencia_Mail
+		WHERE t.Agencia_Direccion IS NOT NULL
+		  AND t.Agencia_Localidad IS NOT NULL
+		  AND t.Agencia_Provincia IS NOT NULL
+		  AND t.Agencia_Nro_Agencia IS NOT NULL
+		  AND t.Agencia_Mail IS NOT NULL
+		  AND t.Agencia_Telefono IS NOT NULL
+		  AND t.Agente_Legajo IS NOT NULL
+		  AND t.Agente_Nombre IS NOT NULL
+		  AND t.Agente_Apellido IS NOT NULL
+		  AND t.Agente_Mail IS NOT NULL
+		  AND t.Agente_Dni IS NOT NULL
+		  AND t.Agente_Fecha_Nac IS NOT NULL
+		  AND t.Agente_Telefono IS NOT NULL
+	END
+GO
+
+CREATE PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Clientes
+ AS
+	BEGIN
+		INSERT INTO GANEN_LA_CUARTA_O_NO_VUELVAN.Cliente(Cliente_Nombre, Cliente_Apellido, Cliente_DNI, Cliente_Fecha_Nac, Cliente_Telefono, Cliente_Mail, Cliente_ID_Direccion)
+		SELECT DISTINCT t.Cliente_Nombre, t.Cliente_Apellido, t.Cliente_Dni, t.Cliente_Fecha_Nac, t.Cliente_Tel, t.Cliente_Mail, d.Direccion_ID
+		FROM gd_esquema.Maestra t
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Provincia p ON p.Provincia_Nombre = t.Cliente_Provincia
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Localidad l on l.Localidad_ID_Provincia = p.Provincia_ID and l.Localidad_Nombre = t.Cliente_Localidad
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Direccion d ON d.Direccion_ID_Localidad = l.Localidad_ID and d.Direccion = t.Cliente_Direccion
+		WHERE t.Cliente_Direccion IS NOT NULL
+		  AND t.Cliente_Localidad IS NOT NULL
+		  AND t.Cliente_Provincia IS NOT NULL
+		  AND t.Cliente_Nombre IS NOT NULL
+		  AND t.Cliente_Apellido IS NOT NULL
+		  AND t.Cliente_Mail IS NOT NULL
+		  AND t.Cliente_Dni IS NOT NULL
+		  AND t.Cliente_Fecha_Nac IS NOT NULL
+		  AND t.Cliente_Tel IS NOT NULL
+	END
+GO
+
+CREATE PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Solicitudes 
+ AS
+	BEGIN
+		INSERT INTO GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud(Solicitud_Nro_Solicitud, Solicitud_Fecha_Solicitud, Solicitud_Fecha_Inicio_Tentativa, Solicitud_Fecha_Fin_Tentativa, Solicitud_Cantidad_Pasajeros, Solicitud_Observaciones, Solicitud_Presupuesto_Estimado, Solicitud_ID_Cliente, Solicitud_ID_Agente)
+		SELECT DISTINCT  t.Solicitud_Nro_Solicitud, t.Solicitud_Fecha_Solicitud, t.Solicitud_Fecha_Inicio_Tentativa, t.Solicitud_Fecha_Fin_Tentativa, t.Solicitud_Cant_Pax, ISNULL(t.Solicitud_Observaciones,''), t.Solicitud_Presupuesto_Estimado, c.Cliente_ID, a.Agente_ID
+		FROM  gd_esquema.Maestra t 
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Cliente c
+			ON c.Cliente_Nombre = t.Cliente_Nombre
+			AND c.Cliente_Apellido = t.Cliente_Apellido
+			AND c.Cliente_DNI = t.Cliente_Dni
+			AND c.Cliente_Mail = t.Cliente_Mail
+			AND c.Cliente_Fecha_Nac = t.Cliente_Fecha_Nac
+			AND c.Cliente_Telefono = t.Cliente_Tel
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Agente a
+			ON a.Agente_DNI = t.Agente_Dni
+			AND a.Agente_Nombre = t.Agente_Nombre
+			AND a.Agente_Apellido = t.Agente_Apellido
+			AND a.Agente_Fecha_Nac = t.Agente_Fecha_Nac
+			AND a.Agente_Legajo = t.Agente_Legajo
+			AND a.Agente_Mail = t.Agente_Mail
+			AND a.Agente_Telefono = t.Agente_Telefono
+		WHERE t.Solicitud_Nro_Solicitud IS NOT NULL 
+			AND t.Solicitud_Fecha_Solicitud IS NOT NULL
+			AND t.Solicitud_Fecha_Inicio_Tentativa IS NOT NULL
+			AND t.Solicitud_Fecha_Fin_Tentativa IS NOT NULL
+			AND t.Solicitud_Cant_Pax IS NOT NULL
+			AND t.Solicitud_Presupuesto_Estimado IS NOT NULL
+			AND t.Cliente_Nombre IS NOT NULL
+			AND t.Cliente_Apellido IS NOT NULL
+			AND t.Cliente_Mail IS NOT NULL
+			AND t.Cliente_Dni IS NOT NULL
+			AND t.Cliente_Fecha_Nac IS NOT NULL
+			AND t.Cliente_Tel IS NOT NULL
+			AND t.Agente_Legajo IS NOT NULL
+			AND t.Agente_Nombre IS NOT NULL
+			AND t.Agente_Apellido IS NOT NULL
+			AND t.Agente_Mail IS NOT NULL
+			AND t.Agente_Dni IS NOT NULL
+			AND t.Agente_Fecha_Nac IS NOT NULL
+			AND t.Agente_Telefono IS NOT NULL
+	END
+GO
+
+CREATE PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Solicitudes_X_Ciudad
+ AS
+	BEGIN
+		INSERT INTO GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud_X_Ciudad(Solicitud_X_Ciudad_Cantidad_Dias, Solicitud_X_Ciudad_Observaciones, Solicitud_X_Ciudad_ID_Ciudad, Solicitud_X_Ciudad_ID_Solicitud)
+		SELECT DISTINCT t.Detalle_Solicitud_Cant_Dias_Aprox, ISNULL(t.Detalle_Solicitud_Observaciones,''), c.Ciudad_ID, s.Solicitud_ID
+		FROM gd_esquema.Maestra t
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Ciudad c
+			ON c.Ciudad_Nombre = t.Detalle_Solicitud_Ciudad
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud S
+			ON s.Solicitud_Nro_Solicitud = t.Solicitud_Nro_Solicitud
+			AND s.Solicitud_Cantidad_Pasajeros = t.Solicitud_Cant_Pax
+			AND s.Solicitud_Fecha_Fin_Tentativa = t.Solicitud_Fecha_Fin_Tentativa
+			AND s.Solicitud_Fecha_Inicio_Tentativa = t.Solicitud_Fecha_Inicio_Tentativa
+			AND s.Solicitud_Fecha_Solicitud = t.Solicitud_Fecha_Solicitud
+			AND s.Solicitud_Presupuesto_Estimado = t.Solicitud_Presupuesto_Estimado
+		WHERE t.Detalle_Solicitud_Cant_Dias_Aprox IS NOT NULL
+		  AND t.Solicitud_Nro_Solicitud IS NOT NULL 
+		  AND t.Solicitud_Fecha_Solicitud IS NOT NULL
+		  AND t.Solicitud_Fecha_Inicio_Tentativa IS NOT NULL
+		  AND t.Solicitud_Fecha_Fin_Tentativa IS NOT NULL
+		  AND t.Solicitud_Cant_Pax IS NOT NULL
+		  AND t.Solicitud_Presupuesto_Estimado IS NOT NULL
+		  AND t.Detalle_Solicitud_Ciudad IS NOT NULL
+	END
+GO
+
+CREATE PROCEDURE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Propuestas 
+ AS
+	BEGIN
+		INSERT INTO GANEN_LA_CUARTA_O_NO_VUELVAN.Propuesta(Propouesta_Nro_Propouesta, Propuesta_Fecha_Emision, Propuesta_Fecha_Vigencia_Hasta, Propuesta_Fecha_Desde, Propuesta_Fecha_Hasta, Propouesta_Subtotal, Propuesta_Descuento, Propuesta_Importe_Total, Propuesta_ID_Solicitud, Propuesta_ID_Agente, Propuesta_ID_Estado)
+		SELECT DISTINCT  t.Propuesta_Nro_Propuesta, t.Propuesta_Fecha_Emision, t.Propuesta_Vigencia_Hasta, t.Propuesta_Fecha_Desde, t.Propuesta_Fecha_Hasta, t.Propuesta_Subtotal, t.Propuesta_Descuento, t.Propuesta_Importe_Total, s.Solicitud_ID, a.Agente_ID, e.Estado_Propuesta_ID
+		FROM  gd_esquema.Maestra t 
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud s
+			ON s.Solicitud_Nro_Solicitud = t.Solicitud_Nro_Solicitud
+			AND s.Solicitud_Cantidad_Pasajeros = t.Solicitud_Cant_Pax
+			AND s.Solicitud_Fecha_Fin_Tentativa = t.Solicitud_Fecha_Fin_Tentativa
+			AND s.Solicitud_Fecha_Inicio_Tentativa = t.Solicitud_Fecha_Inicio_Tentativa
+			AND s.Solicitud_Fecha_Solicitud = t.Solicitud_Fecha_Solicitud
+			AND s.Solicitud_Presupuesto_Estimado = t.Solicitud_Presupuesto_Estimado
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Agente a
+			ON a.Agente_DNI = t.Agente_Dni
+			AND a.Agente_Nombre = t.Agente_Nombre
+			AND a.Agente_Apellido = t.Agente_Apellido
+			AND a.Agente_Fecha_Nac = t.Agente_Fecha_Nac
+			AND a.Agente_Legajo = t.Agente_Legajo
+			AND a.Agente_Mail = t.Agente_Mail
+			AND a.Agente_Telefono = t.Agente_Telefono
+		JOIN GANEN_LA_CUARTA_O_NO_VUELVAN.Estado_Propuesta e
+			on e.Estado_Propuesta_Tipo = t.Propuesta_Estado
+		WHERE t.Propuesta_Nro_Propuesta IS NOT NULL
+			AND t.Propuesta_Fecha_Emision IS NOT NULL
+			AND t.Propuesta_Vigencia_Hasta IS NOT NULL
+			AND t.Propuesta_Fecha_Desde IS NOT NULL
+			AND t.Propuesta_Fecha_Hasta IS NOT NULL
+			AND t.Propuesta_Subtotal IS NOT NULL
+			AND t.Propuesta_Descuento IS NOT NULL
+			AND t.Propuesta_Importe_Total IS NOT NULL
+			AND t.Solicitud_Nro_Solicitud IS NOT NULL 
+			AND t.Solicitud_Fecha_Solicitud IS NOT NULL
+			AND t.Solicitud_Fecha_Inicio_Tentativa IS NOT NULL
+			AND t.Solicitud_Fecha_Fin_Tentativa IS NOT NULL
+			AND t.Solicitud_Cant_Pax IS NOT NULL
+			AND t.Solicitud_Presupuesto_Estimado IS NOT NULL
+			AND t.Agente_Legajo IS NOT NULL
+			AND t.Agente_Nombre IS NOT NULL
+			AND t.Agente_Apellido IS NOT NULL
+			AND t.Agente_Mail IS NOT NULL
+			AND t.Agente_Dni IS NOT NULL
+			AND t.Agente_Fecha_Nac IS NOT NULL
+			AND t.Agente_Telefono IS NOT NULL
+			AND t.Propuesta_Estado IS NOT NULL
+	END
+GO
+
 --Ejecucion de Stored Procedures---------------------------
 
  BEGIN TRANSACTION
@@ -360,6 +627,11 @@ GO
 	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Proveedores
 	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Excursiones
 	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Agencias
+	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Agentes
+	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Clientes
+	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Solicitudes
+	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Solicitudes_X_Ciudad
+	EXECUTE GANEN_LA_CUARTA_O_NO_VUELVAN.migrar_Propuestas
 END TRY
 BEGIN CATCH
     ROLLBACK TRANSACTION;
@@ -376,6 +648,11 @@ AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Aerolinea)
 AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Proveedor)
 AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Excursion)
 AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Agencia)
+AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Agente)
+AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Cliente)
+AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud)
+AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Solicitud_X_Ciudad)
+AND EXISTS (SELECT 1 FROM GANEN_LA_CUARTA_O_NO_VUELVAN.Propuesta)
 )
    BEGIN
 	PRINT 'Tablas migradas correctamente.';
