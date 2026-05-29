e1d3f393797a1c51824cc55ac1d1decdff3b615c
FeliPampin <felupampin23@gmail.com>
2026-05-28 23:38:40 -0300

    arreglamos der
    Co-authored-by: santinosommaruga <ssommaruga@frba.utn.edu.ar>

--- trabajo practico/setup/EjecutarScriptTablaMaestra.bat
+++
@@ -1,3 +0,0 @@
-sqlcmd -S SERVIDOR\INSTANCIA -i gd_esquema.Schema.sql  -a 32767 -o resultado_output.txt
-sqlcmd -S SERVIDOR\INSTANCIA -i gd_esquema.Maestra.sql -a 32767 -o resultado_output.txt
-sqlcmd -S SERVIDOR\INSTANCIA -i gd_esquema.Maestra.Table.sql -a 32767 -o resultado_output.txt
